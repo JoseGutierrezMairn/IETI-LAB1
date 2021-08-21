3c6c2a8d4869c5ce25ed9a2e0830c41a1eabb136
Jose Gutierrez Marin <andres.gutierrez-m@mail.escuelaing.edu.co>
2021-08-21 17:25:25 -0500

    Files generated in step 1

---
+++ src/test/java/edu/eci/USERSMICROSERVICERESTFULAPI/UsersMicroserviceRestfulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.USERSMICROSERVICERESTFULAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMicroserviceRestfulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
